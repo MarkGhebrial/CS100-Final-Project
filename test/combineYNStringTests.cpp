3737f6ff629e2b93951ef630d9d260ca6d385d99
HugoWan0504 <twan012@ucr.edu>
2023-05-26 15:17:55 -0700

    Adding scenarios to test

--- test/combineYNStringTests.cpp
+++ test/combineYNStringTests.cpp
@@ -8,7 +8,12 @@
 
 using namespace std;
 
-TEST (combineYNStringTests, branchName) {
+///Scenarios Tests
+//Created Branch : YES : Enter Branch Name : String
+//Enter commit message : String : Sync changes with remote : YES
+
+/*
+TEST (stringPromptTests, testSingleWordInput) {
     ostringstream out; // This will hold what the prompt prints
     istringstream in("MarkTheCSGod\n"); // This is what the user types
 
@@ -18,3 +23,15 @@ TEST (combineYNStringTests, branchName) {
     EXPECT_EQ(out.str(), "This is a test string prompt: ");
     EXPECT_EQ(result, "MarkTheCSGod");
 }
+
+TEST (YesOrNoPromptTests, testYes) {
+    ostringstream out; // This will hold what the prompt prints
+    istringstream in("yes\n"); // This is what the user types
+
+    YesOrNoPrompt prompt("This is a test prompt", YesOrNo::YES);
+    YesOrNo result = prompt.presentPrompt(out, in);
+
+    EXPECT_EQ(out.str(), "This is a test prompt (Y/n): "); // Check that it printed the right thing
+    EXPECT_EQ(result, YesOrNo::YES); // Check that it returned the right thing
+}
+*/
