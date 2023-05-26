66637fedbec6c86462f7e06542f239a3a4fae97e
HugoWan0504 <twan012@ucr.edu>
2023-05-26 09:11:32 -0700

    Add combineYNString.cpp .h and tests, Update CMakeLists.txt

---
+++ src/ui/combineYNString.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#include "YesOrNoPrompt.h"
+#include "stringPrompt.h"
+
---
+++ test/combineYNStringTests.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include "../src/ui/combineYNString.h"
+#include <string>
+
+using namespace std;
