59ec024bb8ec9b3cfd208692e4736f3db90ccba9
HugoWan0504 <twan012@ucr.edu>
2023-05-26 15:09:19 -0700

    Remove combineYNString.cpp and .h, refit CMakeLists.txt, Update libraries in combineYNStringTests.cpp

--- test/combineYNStringTests.cpp
+++ test/combineYNStringTests.cpp
@@ -1,6 +1,20 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include "../src/ui/combineYNString.h"
 #include <string>
+#include <fstream>
+#include <sstream>
+#include "../src/ui/stringPrompt.h"
+#include "../src/ui/combineYNString.h"
 
 using namespace std;
+
+TEST (combineYNStringTests, branchName) {
+    ostringstream out; // This will hold what the prompt prints
+    istringstream in("MarkTheCSGod\n"); // This is what the user types
+
+    StringPrompt prompt("This is a test string prompt");
+    std::string result = prompt.presentPrompt(out, in);
+
+    EXPECT_EQ(out.str(), "This is a test string prompt: ");
+    EXPECT_EQ(result, "MarkTheCSGod");
+}
