be4c9347dab6f9f53319ecadd28c37f33de1d13d
MarkGhebrial <mghebrialjr@icloud.com>
2023-06-06 15:19:03 -0700

    Move menuPromptTest

---
+++ src/menuPromptTest.cpp
@@ -0,0 +1,28 @@
+#include "ui/menuPrompt.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    vector <string> menuPrompts;
+
+    menuPrompts.push_back("Prompt 1");
+    menuPrompts.push_back("Prompt 2");
+    menuPrompts.push_back("Prompt 3");
+    menuPrompts.push_back("Prompt 4");
+    menuPrompts.push_back("Prompt 5");
+    menuPrompts.push_back("Prompt 6");
+    menuPrompts.push_back("Prompt 7");
+    menuPrompts.push_back("Prompt 8");
+
+    MenuPrompt menuObject(menuPrompts);
+
+    vector<string> userSelection = menuObject.presentPrompt();;
+
+    for (auto s : userSelection) {
+        cout << s << endl;
+    }
+
+    return 0;
+}
