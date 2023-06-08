7a9269ddad6f625dc657b7d1cf9de36685fcc608
MarkGhebrial <mghebrialjr@icloud.com>
2023-06-08 12:49:44 -0700

    Begin implementing the commit wizard

---
+++ src/git/gitCommands.h
@@ -0,0 +1,13 @@
+#ifndef _COMMANDS_H_
+#define _COMMANDS_H_
+
+#include "gitCommand.h"
+
+#include "gitError.h"
+
+#include "gitAdd.h"
+#include "gitCommit.h"
+#include "gitPull.h"
+#include "gitStatus.h"
+
+#endif
--- src/wizards.h
+++ src/wizards.h
@@ -1,8 +1,73 @@
 // This file will only be included once (in main.cpp), so the functions can be defined inline
 
 #include "ui/prompts.h"
+#include "git/gitCommands.h"
 
-void commitWizard() {}
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void commitWizard() {
+    GitStatusResult result = GitStatus().run();
+
+    // TODO: make this code nicer
+    if (result.unstagedChanges.size() == 0) {
+        cout << "Unstaged Changes: none" << endl;
+    } else {
+        cout << "Unstaged Changes: " << endl;
+        for (string s : result.unstagedChanges) {
+            cout << "    " << s << endl;
+        }
+    }
+
+    if (result.untrackedChanges.size() == 0) {
+        cout << "Untracked Changes: none" << endl;
+    } else {
+        cout << "Untracked Changes: " << endl;
+        for (string s : result.untrackedChanges) {
+            cout << "    " << s << endl;
+        }
+    }
+
+    if (result.stagedChanges.size() == 0) {
+        cout << "Staged Changes: none" << endl;
+    } else {
+        cout << "Staged Changes: " << endl;
+        for (string s : result.stagedChanges) {
+            cout << "    " << s << endl;
+        }
+    }
+
+    GitAdd addCommand;
+
+    // Ask the user if they want to stage all changes
+    auto stageAllChangesPrompt = YesOrNoPrompt("Do you want to stage all changes?", YesOrNo::YES);
+    if (stageAllChangesPrompt.presentPrompt(cout, cin) == YesOrNo::NO) {
+        // If not, let them select the files they want to stage
+
+        // Combine the vectors of unstaged and untracked files into menuItems
+        vector<string> menuItems;
+        for (string s : result.unstagedChanges) {
+            menuItems.push_back(s);
+        }
+        for (string s : result.untrackedChanges) {
+            menuItems.push_back(s);
+        }
+
+        // Create and show a menu to the user
+        MenuPrompt fileMenu = MenuPrompt(menuItems);
+        vector<string> selectedFiles = fileMenu.presentPrompt();
+
+        if (selectedFiles.size() == 0) {
+            cout << "No files selected, exiting." << endl;
+            return;
+        }
+
+        addCommand = GitAdd(selectedFiles);
+    } else {
+        addCommand = GitAdd();
+    }
+}
 
 void mergeWizard() {}
 
